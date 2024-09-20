191359d20ca925e5291c5063f9c50a326c62f407
Z Ta <zilvistam@gmail.com>
2024-09-20 20:58:26 +0100

    style improvements continue

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,21 @@
+import { StatusBar } from "expo-status-bar";
 import { Fragment } from "react";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, ViewStyle } from "react-native";
 
 export default function Index() {
   const valuet = () => {};
 
-  interface Props {
+  interface InputProps {
     value: string;
   }
 
-  const CustomUserInput: React.FC<Props> = ({ value }) => {
+  interface DisplayBoxProps {
+    text: string;
+    allowInput?: boolean;
+    style?: ViewStyle;
+  }
+
+  const CustomUserInput: React.FC<InputProps> = ({ value }) => {
     return (
       <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
         <Text>{value}</Text>
@@ -20,55 +27,70 @@ export default function Index() {
     );
   };
 
+  const DisplayBox: React.FC<DisplayBoxProps> = ({ text, allowInput = true, style }) => {
+    return (
+      <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
+        <Text>{text}</Text>
+        <TextInput
+          style={{ borderColor: "gray", borderWidth: 1, width: 40, height: 25, ...style }}
+          keyboardType="numeric"
+          readOnly={!allowInput}
+          pointerEvents={allowInput ? "auto" : "none"}
+        ></TextInput>
+      </View>
+    );
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <Fragment>
+      <Text
+        style={{
+          borderWidth: 1,
+          borderColor: "grey",
+          backgroundColor: "#ececec",
+          height: 50,
+          textAlignVertical: "center",
+          paddingLeft: 10,
+        }}
+      >
+        Tiapa Calc
+      </Text>
       <View
         style={{
           flex: 1,
-          marginTop: 10,
-          width: 100,
-          padding: 0,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <CustomUserInput value="£20" />
-        <CustomUserInput value="£10" />
-        <CustomUserInput value="£5" />
-        <CustomUserInput value="£2" />
-        <CustomUserInput value="£1" />
-        <CustomUserInput value="50p" />
-        <CustomUserInput value="20p" />
-        <CustomUserInput value="10p" />
-        <CustomUserInput value="5p" />
-        <CustomUserInput value="2p" />
-        <CustomUserInput value="1p" />
+        <View
+          style={{
+            flex: 1,
+            marginTop: 10,
+            width: 100,
+            paddingLeft: 0,
+          }}
+        >
+          <CustomUserInput value="£20" />
+          <CustomUserInput value="£10" />
+          <CustomUserInput value="£5" />
+          <CustomUserInput value="£2" />
+          <CustomUserInput value="£1" />
+          <CustomUserInput value="50p" />
+          <CustomUserInput value="20p" />
+          <CustomUserInput value="10p" />
+          <CustomUserInput value="5p" />
+          <CustomUserInput value="2p" />
+          <CustomUserInput value="1p" />
 
-        <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
-          <Text style={{ paddingRight: 5 }}>Total</Text>
-          <Text style={{ borderColor: "black", borderWidth: 1, width: 40, height: 25 }}></Text>
-        </View>
-
-        <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
-          <Text>Expected</Text>
-          <TextInput
-            style={{ borderColor: "gray", borderWidth: 1, width: 40, height: 25 }}
-            keyboardType="numeric"
-          ></TextInput>
-        </View>
-
-        <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
-          <Text>Difference</Text>
-          <TextInput
-            style={{ borderColor: "gray", borderWidth: 1, width: 40, height: 25 }}
-            keyboardType="numeric"
-          ></TextInput>
+          <DisplayBox
+            text="Total"
+            allowInput={false}
+            style={{ borderWidth: 1, borderColor: "black" }}
+          />
+          <DisplayBox text="Expected" />
+          <DisplayBox text="Difference" allowInput={false} />
         </View>
       </View>
-    </View>
+    </Fragment>
   );
 }
