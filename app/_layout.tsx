61c441a087ac2926a061cd407728f3fedb5e18fa
Z Ta <zilvistam@gmail.com>
2024-09-20 20:58:52 +0100

    removed 'index' header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="calc" />
-    </Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    ></Stack>
   );
 }
