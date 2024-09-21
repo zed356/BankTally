189a0387b73719761b53d392dae8aa40d39f0196
Z Ta <zilvistam@gmail.com>
2024-09-21 10:44:52 +0100

    added totals, improved styling

--- app/components/CustomUserInput.tsx
+++ app/components/CustomUserInput.tsx
@@ -1,12 +1,40 @@
-import { View, Text, TextInput, ViewStyle } from "react-native";
+import { useRef, useState } from "react";
+import { View, Text, TextInput, ViewStyle, TextInputAndroidProps } from "react-native";
 
 interface InputProps {
   label: string;
+  value?: string;
   allowInput?: boolean;
   style?: ViewStyle;
+  onBlur?: (target: string, value: string) => void;
+  maxLength?: number;
+  validateUserInput?: boolean;
 }
 
-const CustomUserInput: React.FC<InputProps> = ({ label, allowInput = true, style }) => {
+const CustomUserInput: React.FC<InputProps> = ({
+  label,
+  allowInput = true,
+  style,
+  value,
+  onBlur = () => {},
+  maxLength = 7,
+  validateUserInput = true,
+}) => {
+  const [focused, setFocused] = useState(false);
+
+  const [val, setVal] = useState("0");
+
+  const handleVal = (newVal: string) => {
+    setVal(newVal);
+  };
+
+  const handleFocused = () => {
+    setFocused(!focused);
+  };
+
+  const validateInput = (): boolean =>
+    validateUserInput && Number(val) % Number(label.slice(1)) == 0;
+
   return (
     <View
       style={{
@@ -18,10 +46,27 @@ const CustomUserInput: React.FC<InputProps> = ({ label, allowInput = true, style
     >
       <Text>{label}</Text>
       <TextInput
-        style={{ height: 30, borderColor: "gray", borderWidth: 1, width: 40, ...style }}
-        keyboardType="numeric"
+        style={{
+          height: 30,
+          borderColor: Number(val) != 0 && !validateInput() ? "red" : "gray",
+          borderWidth: 1,
+          width: 60,
+          paddingLeft: 2,
+          pointerEvents: allowInput ? "auto" : "none",
+          ...style,
+        }}
+        placeholderTextColor={focused ? "transparent" : "black"}
+        onFocus={handleFocused}
+        onBlur={() => {
+          handleFocused();
+          onBlur(label, val);
+        }}
+        inputMode="numeric"
         readOnly={!allowInput}
-        pointerEvents={allowInput ? "auto" : "none"}
+        placeholder="0"
+        onChangeText={handleVal}
+        value={value}
+        maxLength={maxLength}
       />
     </View>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,45 @@
-import { Fragment } from "react";
-import { Text, View, TextInput, ViewStyle } from "react-native";
+import { Fragment, useState } from "react";
+import { Text, View } from "react-native";
 
 import CustomUserInput from "./components/CustomUserInput";
 
 export default function Index() {
-  const valuet = () => {};
+  const [values, setValues] = useState({
+    "£20": 0,
+    "£10": 0,
+  });
+
+  const [expectedValue, setExpectedValue] = useState(0);
+
+  const handleExpectedValue = (label: string, val: string) => {
+    setExpectedValue(Number(val));
+  };
+
+  const handleValues = (target: string, value: string) => {
+    const newValues = {
+      ...values,
+      [target]: Number(value),
+    };
+
+    setValues(newValues);
+  };
+
+  const displayTotalValue = Object.values(values)
+    .reduce((acc, cur) => acc + cur)
+    .toString();
+
+  const displayDifference = (Number(displayTotalValue) - expectedValue).toString();
+
+  const handleDifferenceBorder = () => {
+    const temp = Number(displayDifference);
+    if (temp > 0) {
+      return "blue";
+    } else if (temp < 0) {
+      return "red";
+    } else {
+      return "green";
+    }
+  };
 
   return (
     <Fragment>
@@ -14,7 +49,7 @@ export default function Index() {
           borderColor: "grey",
           backgroundColor: "#ececec",
           height: 50,
-          textAlignVertical: "center",
+          verticalAlign: "middle",
           paddingLeft: 10,
         }}
       >
@@ -31,29 +66,40 @@ export default function Index() {
           style={{
             flex: 1,
             marginTop: 10,
-            width: 110,
+            width: 125,
             paddingLeft: 0,
           }}
         >
-          <CustomUserInput label="£20" />
-          <CustomUserInput label="£10" />
-          <CustomUserInput label="£5" />
-          <CustomUserInput label="£2" />
-          <CustomUserInput label="£1" />
-          <CustomUserInput label="50p" />
-          <CustomUserInput label="20p" />
-          <CustomUserInput label="10p" />
-          <CustomUserInput label="5p" />
-          <CustomUserInput label="2p" />
-          <CustomUserInput label="1p" />
-
+          <CustomUserInput label="£20" onBlur={handleValues} />
+          <CustomUserInput label="£10" onBlur={handleValues} />
+          <CustomUserInput label="£5" onBlur={handleValues} />
+          <CustomUserInput label="£2" onBlur={handleValues} />
+          <CustomUserInput label="£1" onBlur={handleValues} />
+          <CustomUserInput label="50p" onBlur={handleValues} />
+          <CustomUserInput label="20p" onBlur={handleValues} />
+          <CustomUserInput label="10p" onBlur={handleValues} />
+          <CustomUserInput label="5p" onBlur={handleValues} />
+          <CustomUserInput label="2p" onBlur={handleValues} />
+          <CustomUserInput label="1p" onBlur={handleValues} />
           <CustomUserInput
             label="Total"
             allowInput={false}
             style={{ borderWidth: 1, borderColor: "black" }}
+            value={displayTotalValue}
+            validateUserInput={false}
           />
-          <CustomUserInput label="Expected" />
-          <CustomUserInput label="Difference" allowInput={false} style={{ borderColor: "blue" }} />
+          <CustomUserInput
+            label="Expected"
+            onBlur={handleExpectedValue}
+            validateUserInput={false}
+          />
+          <CustomUserInput
+            label="Difference"
+            allowInput={false}
+            style={{ borderWidth: 1, borderColor: handleDifferenceBorder() }}
+            value={displayDifference}
+            validateUserInput={false}
+          />
         </View>
       </View>
     </Fragment>
