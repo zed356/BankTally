2bfa57a98f2d995daf60e463e8f196beddef8e8f
z <zilvistam@gmail.com>
2024-12-01 09:47:11 +0000

    added inputlabels from userinputlist

---
+++ constants/Values.ts
@@ -0,0 +1,20 @@
+import { ICurrencyObject } from "@/types/UserInputTypes";
+
+export const defaultValues: ICurrencyObject = {
+  "£20": "",
+  "£10": "",
+  "£5": "",
+  "£2": "",
+  "£1": "",
+  "50p": "",
+  "20p": "",
+  "10p": "",
+  "5p": "",
+  "2p": "",
+  "1p": "",
+  Expected: "",
+  Difference: "",
+  Total: "",
+};
+
+export const inputLabels = ["£20", "£10", "£5", "£2", "£1", "50p", "20p", "10p", "5p", "2p", "1p"];
