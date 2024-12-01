1c12064616d74c2fb709787ed4a78e657c569647
z <zilvistam@gmail.com>
2024-12-01 12:52:50 +0000

    refactored file layout

--- components/user-inputs/UserInputList.tsx
+++ components/user-inputs/UserInputList.tsx
@@ -1,40 +1,44 @@
-import { useEffect, useState } from "react";
-import { View, Pressable, Text, StyleSheet } from "react-native";
-import { writeUserInputValuesToStorage } from "@/stores/PersistentStorage";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import { defaultValues, inputLabels } from "@/constants/Values";
+import {
+  getUserInputValuesFromStorage,
+  writeUserInputValuesToStorage,
+} from "@/stores/PersistentStorage";
 import { ICurrencyObject } from "@/types/UserInputTypes";
-import { getUserInputValuesFromStorage } from "@/stores/PersistentStorage";
-import CustomUserInput from "./CustomUserInput";
-import { defaultValues } from "@/constants/DefaultUserInputValues";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useCallback, useEffect, useState } from "react";
+import { Linking, Pressable, StyleSheet, Text, View } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import ErrorModal from "../ErrorModal";
-import ThermalPrinterModule from "react-native-thermal-printer";
+import { BluetoothPrinter } from "../helperFunctions/BluetoothPrinter";
+import { DisplayTotalValue } from "../helperFunctions/DisplayTotalValue";
+import CustomUserInput from "./CustomUserInput";
+import _ from "lodash";
+import BluetoothStateManager from "react-native-bluetooth-state-manager";
 
 const UserInputList: React.FC = () => {
   const [values, setValues] = useState(defaultValues);
   const [showErrorModal, setShowErrorModal] = useState(false);
+  const [errorModalMessage, setErrorModalMessage] = useState("");
 
-  const displayTotalValue = Object.entries(values).reduce((acc, cur) => {
-    let temp = acc;
-    if (cur[0] != "Expected" && cur[0] != "Difference" && cur[0] != "Total") {
-      if (cur[1] == ".") {
-        temp = acc;
-      } else {
-        temp = acc + +cur[1];
+  // loads old data if there is any upon app start
+  useEffect(() => {
+    (async () => {
+      try {
+        setValues(await getUserInputValuesFromStorage());
+      } catch (error) {
+        setErrorModalMessage("Failed to load data from storage.");
+        setShowErrorModal(true);
       }
-    }
-    return temp;
-  }, 0);
-  const displayDifference = (displayTotalValue - +values.Expected).toFixed(2);
+    })();
+  }, []);
 
-  const printerPayload =
-    "[L]<font size='big'>   Cash breakdown</font>\n" +
-    "\n" +
-    "[L][L][L]<font size='big'>[ ]</font>" +
-    "[C][C][C][C][C][C]<font size='big'>20</font>" +
-    "[R][R][R][R][R][R]<font size='big'>200</font>";
+  const displayTotalValue = DisplayTotalValue(values);
+
+  // convert to ints before calculations to avoid floating point errors
+  const displayDifference = ((displayTotalValue * 100 - +values.Expected * 100) / 100).toFixed(2);
 
   const clearButtonOpacity = useSharedValue(1); // initial opacity
+
   const printButtonOpacity = useSharedValue(1); // initial opacity
 
   const clearButtonAnimatedStyle = useAnimatedStyle(() => {
@@ -48,74 +52,6 @@ const UserInputList: React.FC = () => {
       opacity: printButtonOpacity.value,
     };
   });
-
-  // loads old data if there is any upon app start
-  useEffect(() => {
-    (async () => {
-      try {
-        setValues(await getUserInputValuesFromStorage());
-      } catch (error) {
-        setShowErrorModal(true);
-      }
-    })();
-  }, []);
-
-  const handlePrint = async () => {
-    // initialize printer connection; first run will ask for permission
-    ThermalPrinterModule.getBluetoothDeviceList();
-
-    // keys under [0], values under [1]
-    const valuesToArray = Object.entries(values);
-
-    // filter out values that are not in the inputLabels array
-    const valuesWithoutDifferenceTotalExpected = valuesToArray.filter((value) =>
-      inputLabels.includes(value[0])
-    );
-
-    // filter out values that are empty and apply markdown to the rest
-    const tempPayload = valuesWithoutDifferenceTotalExpected
-      .map((value) => {
-        if (value[1].length > 0) {
-          let denomination;
-          if (value[0][0] == "£") {
-            denomination = value[0].slice(1);
-          } else {
-            denomination = value[0];
-          }
-          return (
-            "[L]<font size='big'>[ ]</font>" +
-            `[C]<font size='big'>${denomination}</font>` +
-            `[R][R]<font size='big'>${value[1]}</font>`
-          );
-        }
-      })
-      .filter((value) => value != undefined);
-
-    // add title
-    tempPayload.unshift("[L]<font size='big'> Cash breakdown</font>\n");
-
-    // add total
-    tempPayload.push(
-      "[C]<font size='big'> Total:</font>" +
-        `[R]<font size='big'>  ${displayTotalValue.toString()}</font>\n` +
-        "\n" +
-        "\n" +
-        "\n" +
-        `.`
-    );
-
-    // join all elements with a newline
-    const payload = tempPayload.join("\n" + "\n");
-
-    try {
-      await ThermalPrinterModule.printBluetooth({
-        payload: payload,
-        printerNbrCharactersPerLine: 30,
-      });
-    } catch (e) {
-      console.log("error printing");
-    }
-  };
 
   const handleValues = (target: keyof ICurrencyObject, value: string) => {
     const newValues = {
@@ -146,40 +82,31 @@ const UserInputList: React.FC = () => {
     }
   };
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: "row",
-      paddingTop: 35,
-      backgroundColor: "#e7eaf6",
-    },
-    inputList: {
-      flex: 1,
-      marginLeft: 105,
-      marginRight: 0,
-      height: "95%",
-    },
-    buttonsContainer: {
-      flex: 1,
-      justifyContent: "space-around",
-      alignItems: "center",
-      width: "100%",
-    },
+  const handlePrinter = _.debounce(async () => {
+    try {
+      await BluetoothPrinter(values, displayTotalValue);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        setErrorModalMessage(e.message);
+        setShowErrorModal(true);
+      } else {
+        console.log("Unexpected error:\n", e);
+      }
+    }
+  }, 1000);
 
-    button: {
-      alignItems: "center",
-      justifyContent: "center",
-      width: "100%",
-      height: 30,
-      paddingHorizontal: 20,
-      borderWidth: 1,
-      borderRadius: 10,
-      userSelect: "none",
-      backgroundColor: "#D1D9DF",
-    },
-  });
-
-  const inputLabels = ["£20", "£10", "£5", "£2", "£1", "50p", "20p", "10p", "5p", "2p", "1p"];
+  const errorHandler = (message: string) => {
+    switch (message) {
+      case "Bluetooth Device Not Found":
+        BluetoothStateManager.openSettings();
+        break;
+      case "Please enable 'Bluetooth/Find nearby devices' permissions in settings.":
+        Linking.openSettings();
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -191,7 +118,7 @@ const UserInputList: React.FC = () => {
           label="Total"
           allowInput={false}
           style={{ borderWidth: 2, borderColor: "black" }}
-          value={displayTotalValue.toString()}
+          value={displayTotalValue.toFixed(2)}
           validateUserInput={false}
           values={values}
         />
@@ -229,7 +156,7 @@ const UserInputList: React.FC = () => {
             style={styles.button}
             onPressIn={() => (printButtonOpacity.value = withTiming(0.5, { duration: 100 }))}
             onPressOut={() => (printButtonOpacity.value = withTiming(1, { duration: 100 }))}
-            onPress={handlePrint}
+            onPress={handlePrinter}
           >
             <Text>Print</Text>
           </Pressable>
@@ -237,13 +164,47 @@ const UserInputList: React.FC = () => {
       </View>
       <ErrorModal
         modalVisible={showErrorModal}
-        errorMessage={`Failed to load data from storage.\nAll fields reset.`}
+        errorMessage={errorModalMessage}
         closeErrorModal={() => {
+          errorHandler(errorModalMessage);
           setShowErrorModal(false);
         }}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    paddingTop: 35,
+    backgroundColor: "#e7eaf6",
+  },
+  inputList: {
+    flex: 1,
+    marginLeft: 105,
+    marginRight: 0,
+    height: "95%",
+  },
+  buttonsContainer: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+  },
+
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 30,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderRadius: 10,
+    userSelect: "none",
+    backgroundColor: "#D1D9DF",
+  },
+});
 
 export default UserInputList;
