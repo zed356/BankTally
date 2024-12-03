3cbfc5d0f1ed8f1c020d083aff6cf08ab28c8116
z <zilvistam@gmail.com>
2024-12-03 19:51:26 +0000

    added lineargradient background

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,18 @@
+import { LinearGradient } from "expo-linear-gradient";
 import UserInputList from "../components/user-inputs/UserInputList";
+import { Colors } from "@/constants/Colors";
+import { StyleSheet } from "react-native";
 
 export default function Index() {
-  return <UserInputList />;
+  return (
+    <LinearGradient colors={Colors.defaultGradient} style={styles.container}>
+      <UserInputList />
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
