82f16ab62e1ca94c2cdfdf17f6d9cc52d85bb90d
z <zilvistam@gmail.com>
2024-12-08 17:50:47 +0000

    moved padStart, padEnd to separate fn

--- components/helperFunctions/PrinterMarkdownWriter.ts
+++ components/helperFunctions/PrinterMarkdownWriter.ts
@@ -1,6 +1,34 @@
 import { inputLabels } from "@/constants/Values";
 import { ICurrencyObject } from "@/types/UserInputTypes";
 
+// pad start / end to make it 7 characters long, then all values line up nicely on the receipt
+const addWhiteSpacesToValue = (value: string) => {
+  let printedValue = value;
+  if (value.includes(".")) {
+    const decimalPart = value.split(".")[1];
+
+    // if 0, means it's just a decimal point with nothing after it
+    // remove it completely and add 3 spaces at the end
+    if (decimalPart.length == 0) {
+      printedValue = value.split(".")[0];
+      printedValue = printedValue.padEnd(printedValue.length + 3, " ");
+    }
+
+    // if only 1 digit after decimal, add 1 space. else it's 2 digits and no space needed
+    if (decimalPart.length == 1) {
+      printedValue = printedValue.padEnd(printedValue.length + 1, " ");
+    }
+    // pad front to make full length 7
+    printedValue = printedValue.padStart(7, " ");
+  } else {
+    // if no decimal point, always 3 spaces needed at the end
+    printedValue = printedValue.padEnd(printedValue.length + 3, " ");
+    // pad front to make full length 7
+    printedValue = printedValue.padStart(7, " ");
+  }
+  return printedValue;
+};
+
 export const PrinterMarkdownWriter = (values: ICurrencyObject, displayTotalValue: number) => {
   // keys under [0], values under [1]
   const valuesToArray = Object.entries(values);
@@ -14,11 +42,11 @@ export const PrinterMarkdownWriter = (values: ICurrencyObject, displayTotalValue
   // skip Difference - Total - Expected as it is not needed here.
   const tempPayload = valuesWithoutDifferenceTotalExpected
     .map(([key, value]: [key: string, value: string]) => {
-      // skip empty values
-      if (value.length == 0) return;
+      // skip empty values, values that are not numbers and 0
+      if (value.length == 0 || isNaN(Number(value)) || value === "0") return;
 
       let denomination;
-      let printedValue = value;
+      let printedValue = addWhiteSpacesToValue(value);
       // skip the £ sign if it exists as printing library does not support it
       if (key[0] == "£") {
         denomination = key.slice(1);
@@ -26,32 +54,8 @@ export const PrinterMarkdownWriter = (values: ICurrencyObject, displayTotalValue
         denomination = key;
       }
 
-      if (value.includes(".")) {
-        const decimalPart = value.split(".")[1];
-
-        // if 0, means it's just a decimal point with nothing after it
-        // remove it completely and add 3 spaces at the end
-        if (decimalPart.length == 0) {
-          printedValue = value.split(".")[0];
-          printedValue = printedValue.padEnd(printedValue.length + 3, " ");
-        }
-
-        // if only 1 digit after decimal, add 1 space. else it's 2 digits and no space needed
-        if (decimalPart.length == 1) {
-          printedValue = printedValue.padEnd(printedValue.length + 1, " ");
-        }
-        // pad front to make full length 7
-        printedValue = printedValue.padStart(7, " ");
-      } else {
-        // if no decimal point, always 3 spaces needed at the end
-        printedValue = printedValue.padEnd(printedValue.length + 3, " ");
-        // pad front to make full length 7
-        printedValue = printedValue.padStart(7, " ");
-      }
-
       return (
-        "[L]<font size='big'>[ ]</font>" +
-        `[L]<font size='big'>${denomination}</font>` +
+        `[L]<font size='big'> ${denomination}</font>` +
         `[R]<font size='big'>${printedValue} </font>`
       );
     })
@@ -63,11 +67,9 @@ export const PrinterMarkdownWriter = (values: ICurrencyObject, displayTotalValue
   // add total
   tempPayload.push(
     "[L]<font size='big'>Total: </font>" +
-      `[R][R]<font size='big'>${displayTotalValue.toFixed(2)}</font>\n` +
+      `[C]<font size='big'>${addWhiteSpacesToValue(displayTotalValue.toFixed(2))}</font>\n` +
       "\n" +
-      "\n" +
-      "\n" +
-      `.`
+      " "
   );
 
   // join all elements with a newline
