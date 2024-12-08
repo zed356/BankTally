dcc60f087ab9cc2e4eded07b70ba45c7602e7d74
z <zilvistam@gmail.com>
2024-12-08 12:19:50 +0000

    improved reusability

--- components/modals/BaseModal.tsx
+++ components/modals/BaseModal.tsx
@@ -1,13 +1,20 @@
+import { DEFAULT_BORDER_RADIUS } from "@/constants/Values";
 import { Modal, StyleSheet, View } from "react-native";
 
 interface BaseModalProps {
   modalVisible: boolean;
   children: React.ReactNode;
+  onRequestClose: () => void;
 }
 
-const BaseModal: React.FC<BaseModalProps> = ({ modalVisible, children }) => {
+const BaseModal: React.FC<BaseModalProps> = ({ children, modalVisible, onRequestClose }) => {
   return (
-    <Modal visible={modalVisible} transparent={true}>
+    <Modal
+      visible={modalVisible}
+      transparent={true}
+      animationType="fade"
+      onRequestClose={onRequestClose}
+    >
       <View style={styles.container}>
         <View style={styles.modalView}>{children}</View>
       </View>
@@ -27,7 +34,7 @@ const styles = StyleSheet.create({
     width: "80%",
     backgroundColor: "#ddf3f1",
     padding: 20,
-    borderRadius: 10,
+    borderRadius: DEFAULT_BORDER_RADIUS,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
